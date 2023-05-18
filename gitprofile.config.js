0edafcb0f78ecf160e9a9050afa2d62a488aecce
THPeace <32593751+thpeace@users.noreply.github.com>
2023-05-18 15:18:43 +0700

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -29,7 +29,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/thpeace/thpeace.github.io/blob/main/public/Sutheekan%20Ratanawatkul%20resume.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Java',
